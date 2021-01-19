6a6ff7767bddf1532cc1ccccc3a300839c297129
Vasanthan Anbu Devan <75612718+vasanthananbu@users.noreply.github.com>
2021-01-19 15:45:57 +0530

    Update Log.js

--- Log.js
+++ Log.js
@@ -2,7 +2,7 @@ class Log{
     constructor(x, y, height, angle){
         var options={
             restitution: 0.8,
-            friction: 1.0,
+            friction: 1.2,
             density: 1.0
         }
         //properties of Box object
